082624069930d979e4facc50db54a0fc9a2a5e6e
Leandro <pnunesleandro@gmail.com>
2024-10-08 21:50:54 -0300

    initial application setup

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tatool</title>
+    <link rel="stylesheet" href="assets/css/application.css">
+</head>
+
+<body>
+    <h1 class="blink">Tatool</h1>
+</body>
+
+</html>
