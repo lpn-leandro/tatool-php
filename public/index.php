f09fc978e66f1cf7a8aace19115a9205c7ddb639
Leandro <pnunesleandro@gmail.com>
2024-11-24 17:26:58 -0300

    Rebase project structure

--- public/index.php
+++ public/index.php
@@ -1,14 +1,3 @@
-<html>
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tatool</title>
-    <link rel="stylesheet" href="assets/css/application.css">
-</head>
-
-<body>
-    <h1 class="blink">Tatool</h1>
-</body>
-
-</html>
+require __DIR__ . '/../config/bootstrap.php';
