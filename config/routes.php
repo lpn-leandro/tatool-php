8ed2f6352789ca24b4ca151625d7b1a9e7a0178b
Leandro <pnunesleandro@gmail.com>
2024-12-02 22:49:09 -0300

    defined some routes

--- config/routes.php
+++ config/routes.php
@@ -5,3 +5,8 @@ use Core\Router\Route;
 
 // Authentication
 Route::get('/', [HomeController::class, 'index'])->name('root');
+
+// Authentication
+Route::get('/login', [AuthenticationController::class, 'new'])->name('users.login');
+Route::post('/login', [AuthenticationController::class, 'authenticate'])->name('users.authenticate');
+Route::get('/logout', [AuthenticationController::class, 'destroy'])->name('users.logout');
