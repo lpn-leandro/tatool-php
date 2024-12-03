730201006c4063e5af573e8d9d700e5f5d3fe6bc
Leandro <108540053+lpn-leandro@users.noreply.github.com>
2024-12-03 17:02:52 +0000

    Defined middleware on route group

--- config/routes.php
+++ config/routes.php
@@ -2,11 +2,17 @@
 
 use App\Controllers\HomeController;
 use Core\Router\Route;
-
-// Authentication
-Route::get('/', [HomeController::class, 'index'])->name('root');
+use App\Controllers\AuthenticationController;
 
 // Authentication
 Route::get('/login', [AuthenticationController::class, 'new'])->name('users.login');
 Route::post('/login', [AuthenticationController::class, 'authenticate'])->name('users.authenticate');
+
+Route::middleware('auth')->group(function () {
+
 Route::get('/logout', [AuthenticationController::class, 'destroy'])->name('users.logout');
+
+// Authentication
+Route::get('/', [HomeController::class, 'index'])->name('root');
+
+});
