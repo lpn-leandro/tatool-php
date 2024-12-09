75d16161bac082e9617098b6c0288a4a42d1b105
Leandro <108540053+lpn-leandro@users.noreply.github.com>
2024-12-09 00:20:34 -0300

    User login (#5)
    * defined some routes
    
    * defined authentication controller
    
    * Defined middleware on route group
    
    * minor fixes on the class name
    
    * changes ou routename to match controller
    
    * fixed SQL error on model
    
    * Defined navbar logout
    
    * fixed some erros on tests

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,15 @@
 
 use App\Controllers\HomeController;
 use Core\Router\Route;
+use App\Controllers\AuthenticationsController;
 
 // Authentication
-Route::get('/', [HomeController::class, 'index'])->name('root');
+Route::get('/login', [AuthenticationsController::class, 'new'])->name('users.login');
+Route::post('/login', [AuthenticationsController::class, 'authenticate'])->name('users.authenticate');
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/logout', [AuthenticationsController::class, 'destroy'])->name('users.logout');
+
+    Route::get('/', [HomeController::class, 'index'])->name('home.index');
+});
