50086f533c5f7c5a68b8613ee0dc11221d807a8a
Leandro <pnunesleandro@gmail.com>
2025-02-23 02:52:16 -0300

    defined access tests

---
+++ tests/Unit/Access/TattooistAppointmentsAccessTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Unit\Access;
+
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+
+class TattooistAppointmentsAccessTest extends TestCase
+{
+    private Client $client;
+
+    public function setup(): void
+    {
+        parent::setUp();
+        $this->client = new Client([
+            'allow_redirects' => false, // Disable following redirects
+            'base_uri' => 'http://web:8080'
+        ]);
+    }
+
+    public function test_should_not_access_the_index_route_if_not_authenticated(): void
+    {
+        $response = $this->client->get('tattooist/appointments');
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login', $response->getHeaderLine('Location'));
+    }
+
+    public function test_should_not_access_the_show_route_if_not_authenticated(): void
+    {
+        $response = $this->client->get('tattooist/appointments/1');
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login', $response->getHeaderLine('Location'));
+    }
+
+    public function test_should_not_access_the_route_route_if_not_authenticated(): void
+    {
+        $response = $this->client->get('tattooist/appointments/new');
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login', $response->getHeaderLine('Location'));
+    }
+
+    public function test_should_not_access_the_create_route_if_not_authenticated(): void
+    {
+        $response = $this->client->post('tattooist/appointments', []);
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login', $response->getHeaderLine('Location'));
+    }
+
+    public function test_should_not_access_the_edit_route_if_not_authenticated(): void
+    {
+        $response = $this->client->get('tattooist/appointments/1/edit');
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login', $response->getHeaderLine('Location'));
+    }
+
+    public function test_should_not_access_the_update_route_if_not_authenticated(): void
+    {
+        $response = $this->client->put('tattooist/appointments/1', []);
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/login', $response->getHeaderLine('Location'));
+    }
+}
